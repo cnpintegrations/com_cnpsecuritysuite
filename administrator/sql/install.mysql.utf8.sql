eabb7e4397b107bed8f02ce5ee30df3a1bb6f84a
Tyler Oliver <tyler8oliver@gmail.com>
2015-11-24 15:23:45 -0500

    added notifications table

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -11,3 +11,11 @@ INSERT INTO `#__cnpsecuritysuite_keys` (`vendor`, `script`) VALUES
 ('Watchfuli', 'plugin_watchfuli'),
 ('Pingdom', 'plugin_pingdom');
 
+CREATE TABLE IF NOT EXISTS `#__cnpsecuritysuite_notify` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+
+`email` VARCHAR(255)  NOT NULL ,
+`name` VARCHAR(255)  NOT NULL, 
+`frequency` INT(11)  NOT NULL 
+PRIMARY KEY (`id`)
+) DEFAULT COLLATE=utf8_general_ci;
